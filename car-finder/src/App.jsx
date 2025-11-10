f687df64fb26044f000be941a66c282e8de4c283
Noah <nbelman1@gmail.com>
2025-11-09 22:11:42 -0500

    Added components for testing

--- car-finder/src/App.jsx
+++ car-finder/src/App.jsx
@@ -3,13 +3,22 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Question from './components/question-assets/Question';
+import { questionSet } from './components/question-assets/QuestionSet';
+import Modal from './components/question-assets/Modal';
+import ContactUs from './components/ContactUs';
+
 
 function App() {
 
 	return (
+		// TODO: add states to components - most are here for testing
     	<div>
       		<Header />
 			<Home />
+			<Question questionSet={questionSet} />
+			<Modal questionSet={questionSet} />
+			<ContactUs />
 			<Footer />
     	</div>
   	)
