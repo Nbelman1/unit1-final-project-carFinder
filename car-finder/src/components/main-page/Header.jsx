c01c0694815bc3aabf24ca6a73ce115a22526be2
Noah <nbelman1@gmail.com>
2025-11-19 20:46:36 -0500

    Added resetQuiz functionality to Links

--- car-finder/src/components/main-page/Header.jsx
+++ car-finder/src/components/main-page/Header.jsx
@@ -1,16 +1,38 @@
 import { Link } from 'react-router';
 
-const Header = () => {
-  return (
+const Header = ({ setUserAnswers, setUserResponse, setCurrentQuestion }) => {
+
+	const resetQuiz = () => {
+		setUserAnswers([]);
+		setUserResponse("");
+		setCurrentQuestion(0);
+	}
+
+	return (
+
     <div>
         <header>
             <div className="nav-container">
-				<h1 className="logo"><Link to="/">CarFinder</Link></h1>
+				
+				<h1 className="logo">
+					<Link 
+						onClick={resetQuiz}
+						to="/"
+					>
+						CarFinder
+					</Link>
+				</h1>
 
                 <nav className="nav-container">
 					<ul className="nav-links">
 						<li className="nav-links">
-							<Link to="/quiz">Start Quiz</Link>
+							{/* TODO: fix bug. button does not reset state of follow up questions. pass setShowingFollowUp prop, reset to false */}
+							<Link
+								onClick={resetQuiz}
+								to="/quiz"
+							>
+								Start Quiz
+							</Link>
 						</li>
 						<li className="nav-links">
 							<Link to="/complete-list">View All Cars</Link>
