c09a9fed0ecb8ce5a9180902f938d9398ec9db13
Noah <nbelman1@gmail.com>
2025-11-26 10:22:33 -0500

    Modified base name for re-deployment

--- car-finder/vite.config.js
+++ car-finder/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'unit1-final-project-carFinder';
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${reponame}`,
+  base: '/unit1-final-project-carFinder/', // repo name
 })
