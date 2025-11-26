258a881a2ef4ed9ba1f89b0f536d1204b7f813d5
Noah <nbelman1@gmail.com>
2025-11-26 10:09:27 -0500

    Prepped for deployment to GH-Pages

--- car-finder/vite.config.js
+++ car-finder/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = 'unit1-final-project-carFinder';
+
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: `/${reponame}`,
 })
